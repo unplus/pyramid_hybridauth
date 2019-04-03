a7c5044fd270a4ff0a8ce1e9e81aec2a42e11b7c
Yoshimitsu Kokubo <yoshi@unplus.net>
2019-04-03 10:01:20 +0900

    Fixed the ninja oauth.

--- README.rst
+++ README.rst
@@ -4,4 +4,4 @@ pyramid_hybridauth README
 Getting Started
 ---------------
 
-- pip install --index=http://pypi.shinobi.vpn/simple pyramid_hybridauth
+- pip install pyramid_hybridauth
--- src/pyramid_hybridauth/services/ninja.py
+++ src/pyramid_hybridauth/services/ninja.py
@@ -1,5 +1,7 @@
 from . import Service, User
+from oauthlib.common import to_unicode
 from requests_oauthlib import OAuth2Session
+from json import dumps
 
 
 class NinjaService(Service):
@@ -14,6 +16,7 @@ class NinjaService(Service):
         oauth = OAuth2Session(
             config.client_id, scope=config.scope, redirect_uri=callback_url
         )
+        oauth = ninja_compliance_fix(oauth)
         oauth.fetch_token(
             config.access_token_url,
             client_secret=config.secret,
@@ -25,3 +28,19 @@ class NinjaService(Service):
         uid = data["id"]
         display_name = data.get("nickname")
         return User(uid, display_name, data)
+
+
+def ninja_compliance_fix(session):
+    def _compliance_fix(r):
+        if r.status_code != 200:
+            return r
+
+        token = r.json()
+        expires_in = token.get("expires_in")
+        if expires_in and int(expires_in) < 1:
+            token.pop("expires_in")
+        r._content = to_unicode(dumps(token)).encode("UTF-8")
+        return r
+
+    session.register_compliance_hook("access_token_response", _compliance_fix)
+    return session
